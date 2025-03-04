fcf41e0ed3565e6098bf36e72754a2273ec0b5f0
Denys Pronin <denispronindev@gmail.com>
2025-03-04 18:19:53 +0200

    Upload to vercel

--- src/marvel-api.js
+++ src/marvel-api.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://gateway.marvel.com:443/v1/public/";
-const apiKey = "apikey=c5d6fc8b83116d92ed468ce36bac6c62";
+const API_KEY = import.meta.env.VITE_MARVEL_API_KEY;
 
 export const getAllCharacters = async () => {
-  const res = await axios.get(`/characters?limit=9&offset=210&${apiKey}`);
+  const res = await axios.get(`/characters`, {
+    params: {
+      limit: 9,
+      offset: 210,
+      apikey: API_KEY,
+    },
+  });
   return res.data.data.results;
 };
 
 export const getCharacter = async (id) => {
-  const res = await axios.get(`/characters/${id}?${apiKey}`);
+  const res = await axios.get(`/characters/${id}`, {
+    params: {
+      apikey: API_KEY,
+    },
+  });
   return res.data.data.results[0];
 };
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
 import { defineConfig } from "vite";
-import { glob } from "glob";
-import react from "@vitejs/plugin-react";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
+import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
-    root: "src",
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync("./src/*.html"),
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return "vendor";
-            }
-          },
-          entryFileNames: "commonHelpers.js",
-        },
-      },
-      outDir: "../dist",
-    },
-    plugins: [react(), injectHTML(), FullReload(["./src/**/**.html"])],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
